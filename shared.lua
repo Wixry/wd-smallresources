9bdb339f9d7d48c15a95b802f3e0d65dc1fc4953
WixryDev <113896743+Wixry@users.noreply.github.com>
2023-02-20 21:51:29 +0100

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -4,4 +4,4 @@ Config.IDCommand = true -- Enables the ability to ask for your id through chat.
 Config.policeManagement = true -- Gives the police heads the ability to fire/hire policeman
 Config.Density = false -- Turn the level of npc's lower or maybe higher if you're a dumb ass ##### WIP
 Config.DensityMultiplier = 0.2 -- Value of rate of npc's
-Config.enginePlof = true -- ##### WIP
+Config.TireFlat = true -- ##### WIP
